bb000c9e9ed99dedaf0b565c215c2d8ab2b19bd9
Caue Felipe Pessoa Silva <cauefelipe446@gmail.com>
2026-05-24 17:31:05 -0300

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-summation/grasshopper-summation.java
@@ -0,0 +1,11 @@
+public class GrassHopper {
+​
+    public static int summation(int n) {
+      int sum = 0;
+      for(int i = 1; i<n+1; i++){
+        sum += i;
+}
+​
+        return sum;
+    }
+}
