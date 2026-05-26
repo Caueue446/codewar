05d3ae10bbed4e6161d71f6b23bbf676550c932c
Caue Felipe Pessoa Silva <cauefelipe446@gmail.com>
2026-05-25 22:25:53 -0300

    Add solution - CodeHub

---
+++ 8-kyu/calculate-bmi/calculate-bmi.java
@@ -0,0 +1,19 @@
+public class Calculate {
+  public static String bmi(double weight, double height) {
+    double bmi = (weight / (height*height));
+    String result = "";
+    if(bmi<=18.5){
+      result = "Underweight";
+}
+    else if(bmi<=25.0){
+      result = "Normal";
+}
+    else if(bmi<=30.0){
+      result = "Overweight";
+}
+    else if(bmi>30.0){
+      result = "Obese";
+}
+    return result;
+  }
+}
