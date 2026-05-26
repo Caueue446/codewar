028f979de06c25f7984dbb4f4d3f26a74a2294cc
Caue Felipe Pessoa Silva <cauefelipe446@gmail.com>
2026-05-26 20:21:19 -0300

    Add solution - CodeHub

---
+++ 8-kyu/convert-number-to-reversed-array-of-digits/convert-number-to-reversed-array-of-digits.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Kata {
+  public static int[] digitize(long n) {
+      return new StringBuilder(String.valueOf(n))
+        .reverse()
+        .chars()
+        .map(Character::getNumericValue)
+        .toArray();
+        
+  }
+}
