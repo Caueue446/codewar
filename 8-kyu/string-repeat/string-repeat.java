75cfc9214ea689c5512a38ca3148d27f04af9c09
Caue Felipe Pessoa Silva <cauefelipe446@gmail.com>
2026-05-27 19:58:52 -0300

    Add solution - CodeHub

---
+++ 8-kyu/string-repeat/string-repeat.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+      if(repeat == 0){
+        return "";
+      }
+      String result = string;
+      for(int i = 1; i < repeat; i++){
+        result += string;
+}
+        return result;
+    }
+}
+​
